af53cf5b769d211b8a5006ff635c05d2a8cb6539
CeyranSultanova <ceyransultanova11@gmail.com>
2022-04-15 20:40:21 +0400

    class

---
+++ classes/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace classes
+{
+    class Person
+    {
+        public string Name;
+        public string SurName;
+        public int Age;
+        public Person(string name,string surname ,int age )
+        {
+            Name = name; SurName = surname; Age = age;
+        }
+
+    }
+
+}
---
+++ classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person1 = new Person("Ceyran", "Sultanova",22);
+            Person person2 = new Person("Ceyran1", "Sultanova2");
+
+            Console.WriteLine(person1.Age);
+            Console.WriteLine(person2.Name);
+           
+            //Person person2 = new Person("Ceyran", "Sultanova", 2);
+        
+            //Console.WriteLine(person1.Age);
+            //Console.WriteLine(person2.Age);
+
+        }
+    }
+
+}
